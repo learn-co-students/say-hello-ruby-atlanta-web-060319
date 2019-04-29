704650f411087eb4d8f9a7190149ff369995d266
aeisha simmons <ashicks2017@outlook.com>
2019-04-29 04:27:58 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
-# Build your say_hello method here
+# Build a method call say_hello
+ def say_hello(name = "Ruby Programmer")
+   puts "Hello #{name}!"
+   
+ end
+ 
+ say_hello ("Aeisha")
+ say_hello
